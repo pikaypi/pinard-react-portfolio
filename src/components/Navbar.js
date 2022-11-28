9c19217533b1d36d9a0dc38a75bf4127c5a738fc
Paul Pinard <paul.k.pinard@gmail.com>
2022-11-28 11:46:09 -0500

    add navbar tabs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,37 @@
 import React from 'react';
-
-function Navbar() {
-  return <p>This is the "Navbar" component</p>
+const styles = {
+  title: {
+    padding: 10
+  }
+}
+function Navbar({ currentPage, handlePageChange }) {
+  return (
+    <nav className='navbar'>
+      <h1 style={styles.title}>Paul Pinard</h1>
+      <ul className='nav nav-tabs justify-content-end'>
+        <li className='nav-item'>
+          <a href='#aboutMe' onClick={() => handlePageChange('aboutMe')} className={currentPage === 'aboutMe' ? 'nav-link active' : 'nav-link'}>
+            About Me
+          </a>
+        </li>
+        <li className='nav-item'>
+          <a href='#contact' onClick={() => handlePageChange('contact')} className={currentPage === 'contact' ? 'nav-link active' : 'nav-link'}>
+            Contact
+          </a>
+        </li>
+        <li className='nav-item'>
+          <a href='#portfolio' onClick={() => handlePageChange('portfolio')} className={currentPage === 'portfolio' ? 'nav-link active' : 'nav-link'}>
+            Portfolio
+          </a>
+        </li>
+        <li className='nav-item'>
+          <a href='#resume' onClick={() => handlePageChange('resume')} className={currentPage === 'resume' ? 'nav-link active' : 'nav-link'}>
+            Resume
+          </a>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar;
