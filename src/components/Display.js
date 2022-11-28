b819cab9f5929ec60dfad57389924df27d9bb25a
Paul Pinard <paul.k.pinard@gmail.com>
2022-11-28 11:02:55 -0500

    initial Display function

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
-function Display() {
-  return (
-    <div>
-        <p>This is the "Display" component which will house the content of the page</p>
-    </div>
-  )
+export default function Display() {
+    const [currentPage, setCurrentPage] = useState('AboutMe');
+
+    const renderPage = () => {
+        if (currentPage === 'AboutMe') {
+            return <AboutMe />;
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+        if (currentPage === 'Resume') {
+            return <Resume />;
+        }            
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+        </div>
+    )
 }
-
-export default Display;
