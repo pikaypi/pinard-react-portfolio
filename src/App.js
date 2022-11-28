b555c5367d752c71f9558f72f06537c0480badd8
Paul Pinard <paul.k.pinard@gmail.com>
2022-11-28 10:29:55 -0500

    temporarily format the app for development

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-// TODO: Add a comment explaining the purpose of the App component
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <AboutMe />
+      <Portfolio />
+      <Contact />
+      <Resume />
+    </div>
+  )
 }
 
 export default App;
