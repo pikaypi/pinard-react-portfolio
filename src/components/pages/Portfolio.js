10b573e4e4ca2fc24df274cfb17212e86579a6e9
Paul Pinard <paul.k.pinard@gmail.com>
2022-11-28 13:23:26 -0500

    basic portfolio page structure

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import PortfolioDisplay from './PortfolioDisplay';
+import SpillinBeans from './projects/SpillinBeans';
 
 function Portfolio() {
-  return <p>This is the "Portfolio" component</p>
+  const [currentProject, setCurrentProject] = useState('spillinBeans');
+
+  const renderProject = () => {
+    switch (currentProject) {
+      case 'spillinBeans':
+        return <SpillinBeans />    
+      default:
+        break;
+    }
+  }
+
+  const handleProjectChange = (project) => setCurrentProject(project)
+  
+  return (
+    <div>
+      {renderProject()}
+      <p>This is the portfolio gallery</p>
+    </div>
+  )
 }
 
 export default Portfolio;
