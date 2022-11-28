94e6119681defc11ebebe29bf37666a890fe8a0e
Paul Pinard <paul.k.pinard@gmail.com>
2022-11-28 10:11:16 -0500

    add Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// TODO: Add a comment explaining what a react component is
+function Navbar() {
+  const name = 'Paul Pinard';
+
+  // TODO: Add a comment explaining what JSX is and the significance of the curly braces
+  return <p>Hello World! Here is {name}</p>;
+}
+
+export default Navbar;
