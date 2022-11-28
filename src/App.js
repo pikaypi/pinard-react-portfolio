55768f270d0989e13a08520996c715cac6e29696
Paul Pinard <paul.k.pinard@gmail.com>
2022-11-28 10:39:35 -0500

    create display to house conditional content

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import AboutMe from './components/AboutMe';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
+import Display from './components/Display';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <AboutMe />
-      <Portfolio />
-      <Contact />
-      <Resume />
+      <Display />
     </div>
   )
 }
---
+++ src/components/Display.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Display() {
+  return (
+    <div>
+        <p>This is the "Display" component which will house the content of the page</p>
+    </div>
+  )
+}
+
+export default Display;
