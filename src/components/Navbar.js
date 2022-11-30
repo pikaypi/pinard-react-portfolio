660e51b36ae83de308c1a6ceb063426b76f8e093
Paul Pinard <paul.k.pinard@gmail.com>
2022-11-29 23:35:04 -0500

    initial style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,28 @@
 import React from 'react';
-const styles = {
-  title: {
-    padding: 10
-  }
-}
+import '../styles/Navbar.css'
+
 function Navbar({ currentPage, handlePageChange }) {
   return (
     <nav className='navbar'>
-      <h1 style={styles.title}>Paul Pinard</h1>
-      <ul className='nav nav-tabs justify-content-end'>
+      <h1>Paul Pinard</h1>
+      <ul className='nav nav-tabs-success justify-content-end'>
         <li className='nav-item'>
-          <a href='#aboutMe' onClick={() => handlePageChange('aboutMe')} className={currentPage === 'aboutMe' ? 'nav-link active' : 'nav-link'}>
+          <a href='#aboutMe' onClick={() => handlePageChange('aboutMe')} className={currentPage === 'aboutMe' ? 'nav-link active text-warning' : 'nav-link text-dark'}>
             About Me
           </a>
         </li>
         <li className='nav-item'>
-          <a href='#contact' onClick={() => handlePageChange('contact')} className={currentPage === 'contact' ? 'nav-link active' : 'nav-link'}>
+          <a href='#contact' onClick={() => handlePageChange('contact')} className={currentPage === 'contact' ? 'nav-link active text-warning' : 'nav-link text-dark'}>
             Contact
           </a>
         </li>
         <li className='nav-item'>
-          <a href='#portfolio' onClick={() => handlePageChange('portfolio')} className={currentPage === 'portfolio' ? 'nav-link active' : 'nav-link'}>
+          <a href='#portfolio' onClick={() => handlePageChange('portfolio')} className={currentPage === 'portfolio' ? 'nav-link active text-warning' : 'nav-link text-dark'}>
             Portfolio
           </a>
         </li>
         <li className='nav-item'>
-          <a href='#resume' onClick={() => handlePageChange('resume')} className={currentPage === 'resume' ? 'nav-link active' : 'nav-link'}>
+          <a href='#resume' onClick={() => handlePageChange('resume')} className={currentPage === 'resume' ? 'nav-link active text-warning' : 'nav-link text-dark'}>
             Resume
           </a>
         </li>
