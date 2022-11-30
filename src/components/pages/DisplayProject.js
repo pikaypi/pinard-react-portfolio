534238f96d4c655d9fdf9cd29b60b0362485d67b
Paul Pinard <paul.k.pinard@gmail.com>
2022-11-30 00:08:09 -0500

    initial project display styling

--- src/components/pages/DisplayProject.js
+++ src/components/pages/DisplayProject.js
@@ -1,8 +1,9 @@
 import React from "react";
+import '../../styles/DisplayProject.css'
 
 export default function DisplayProject(props) {
     return (
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card display-project">
             <a href={props.url} target={"_blank"} rel="noreferrer">
                 <img src={props.image} alt={`A screenshot of the ${props.name} homepage`} className={'card-img-top'} />
             </a>
