3a0148bfdf5cb63901e15bab761d1b9b85987eb0
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-01 11:23:19 -0500

    refactor gallery into bootstrap card

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -8,31 +8,59 @@ import passwordGeneratorImg from '../../images/password-generator.png';
 
 export default function Gallery({ handleProjectChange }) {
     return (
-        <div>
-            <a href="#spillinBeans" onClick={() => handleProjectChange('spillinBeans')}>
-                <img src={spillinBeansImg} alt={`A screenshot of the Spillin' Beans homepage`} height={100}/>
-                <h3>Spillin' Beans</h3>
-            </a>
-            <a href="#weekendPlanner" onClick={() => handleProjectChange('weekendPlanner')}>
-                <img src={weekendPlannerImg} alt={`A screenshot of the Extended Weekend Planner homepage`} height={100}/>
-                <h3>Extended Weekend Planner</h3>
-            </a>
-            <a href="#weatherDashboard" onClick={() => handleProjectChange('weatherDashboard')}>
-                <img src={weatherDashboardImg} alt={`A screenshot of the Weather Dashboard homepage`} height={100}/>
-                <h3>Weather Dashboard</h3>
-            </a>
-            <a href="#dailyPlanner" onClick={() => handleProjectChange('dailyPlanner')}>
-                <img src={dailyPlannerImg} alt={`A screenshot of the Daily Planner homepage`} height={100}/>
-                <h3>Daily Planner</h3>
-            </a>
-            <a href="#multipleChoice" onClick={() => handleProjectChange('multipleChoice')}>
-                <img src={multipleChoiceImg} alt={`A screenshot of the Multiple Choice Quiz homepage`} height={100}/>
-                <h3>Multiple Choice Quiz</h3>
-            </a>
-            <a href="#passwordGenerator" onClick={() => handleProjectChange('passwordGenerator')}>
-                <img src={passwordGeneratorImg} alt={`A screenshot of the Password Generator homepage`} height={100}/>
-                <h3>Password Generator</h3>
-            </a>
+        <div className="container">
+            <div className="row">
+                <div className="card col-4">
+                    <a href="#spillinBeans" onClick={() => handleProjectChange('spillinBeans')}>
+                        <img src={spillinBeansImg} alt={`A screenshot of the Spillin' Beans homepage`} className={'card-img-top'}/>
+                        <div className="card-body">
+                            <h3 className="card-title">Spillin' Beans</h3>
+                        </div>
+                    </a>
+                </div>
+                <div className="card col-4">
+                    <a href="#weekendPlanner" onClick={() => handleProjectChange('weekendPlanner')}>
+                        <img src={weekendPlannerImg} alt={`A screenshot of the Extended Weekend Planner homepage`} className={'card-img-top'}/>
+                        <div className="card-body">
+                            <h3 className="card-title">Extended Weekend Planner</h3>
+                        </div>
+                    </a>
+                </div>
+                <div className="card col-4">
+                    <a href="#weatherDashboard" onClick={() => handleProjectChange('weatherDashboard')}>
+                        <img src={weatherDashboardImg} alt={`A screenshot of the Weather Dashboard homepage`} className={'card-img-top'}/>
+                        <div className="card-body">
+                            <h3 className="card-title">Weather Dashboard</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div className="row">
+                <div className="card col-4">
+                    <a href="#dailyPlanner" onClick={() => handleProjectChange('dailyPlanner')}>
+                        <img src={dailyPlannerImg} alt={`A screenshot of the Daily Planner homepage`} className={'card-img-top'}/>
+                        <div className="card-body">
+                            <h3 className="card-title">Daily Planner</h3>
+                        </div>
+                    </a>
+                </div>
+                <div className="card col-4">
+                    <a href="#multipleChoice" onClick={() => handleProjectChange('multipleChoice')}>
+                        <img src={multipleChoiceImg} alt={`A screenshot of the Multiple Choice Quiz homepage`} className={'card-img-top'}/>
+                        <div className="card-body">
+                            <h3 className="card-title">Multiple Choice Quiz</h3>
+                        </div>
+                    </a>
+                </div>
+                <div className="card col-4">
+                    <a href="#passwordGenerator" onClick={() => handleProjectChange('passwordGenerator')}>
+                        <img src={passwordGeneratorImg} alt={`A screenshot of the Password Generator homepage`} className={'card-img-top'}/>
+                        <div className="card-body">
+                            <h3 className="card-title">Password Generator</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
