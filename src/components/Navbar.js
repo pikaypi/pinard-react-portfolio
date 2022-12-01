79b9ab59cc56c4899661c10f3db66efdb255632e
Paul Pinard <paul.k.pinard@gmail.com>
2022-12-01 16:10:04 -0500

    change active tab color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,22 +7,22 @@ function Navbar({ currentPage, handlePageChange }) {
       <h1>Paul Pinard</h1>
       <ul className='nav nav-tabs-success justify-content-end'>
         <li className='nav-item'>
-          <a href='#aboutMe' onClick={() => handlePageChange('aboutMe')} className={currentPage === 'aboutMe' ? 'nav-link active text-warning' : 'nav-link text-dark'}>
+          <a href='#aboutMe' onClick={() => handlePageChange('aboutMe')} className={currentPage === 'aboutMe' ? 'nav-link active' : 'nav-link text-dark'}>
             About Me
           </a>
         </li>
         <li className='nav-item'>
-          <a href='#contact' onClick={() => handlePageChange('contact')} className={currentPage === 'contact' ? 'nav-link active text-warning' : 'nav-link text-dark'}>
+          <a href='#contact' onClick={() => handlePageChange('contact')} className={currentPage === 'contact' ? 'nav-link active' : 'nav-link text-dark'}>
             Contact
           </a>
         </li>
         <li className='nav-item'>
-          <a href='#portfolio' onClick={() => handlePageChange('portfolio')} className={currentPage === 'portfolio' ? 'nav-link active text-warning' : 'nav-link text-dark'}>
+          <a href='#portfolio' onClick={() => handlePageChange('portfolio')} className={currentPage === 'portfolio' ? 'nav-link active' : 'nav-link text-dark'}>
             Portfolio
           </a>
         </li>
         <li className='nav-item'>
-          <a href='#resume' onClick={() => handlePageChange('resume')} className={currentPage === 'resume' ? 'nav-link active text-warning' : 'nav-link text-dark'}>
+          <a href='#resume' onClick={() => handlePageChange('resume')} className={currentPage === 'resume' ? 'nav-link active' : 'nav-link text-dark'}>
             Resume
           </a>
         </li>
